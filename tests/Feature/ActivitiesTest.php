4dc0f81775fecc12b2f6be6aa1d80a74184d0e86
Shakil Alam <itxshakiil@gmail.com>
2020-01-13 20:32:49 +0530

    Extracting Activity Feed query to Model

--- tests/Feature/ActivitiesTest.php
+++ tests/Feature/ActivitiesTest.php
@@ -6,6 +6,7 @@ use App\Activity;
 use App\Reply;
 use App\Thread;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -44,5 +45,29 @@ class ActivitiesTest extends TestCase
         $reply = factory(Reply::class)->create();
 
         $this->assertEquals(2, Activity::count());
+    }
+
+    /**
+    * @test
+    */
+    public function it_fetches_a_feed_for_the_user()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        factory(Thread::class, 2)->create([
+            'user_id' => auth()->id()
+        ]);
+
+        auth()->user()->activities()->first()->update(['created_at' => Carbon::now()->subWeek()->format('Y-m-d')]);
+
+        $feed = Activity::feed(auth()->user());
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->format('Y-m-d')
+        ));
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->subWeek()->format('Y-m-d')
+        ));
     }
 }
