0a83a54c6b38a788e6659e74ec63da05d447cc09
Shakil Alam <itxshakiil@gmail.com>
2020-01-20 13:19:40 +0530

    Paginate All Threads Page

---
+++ resources/views/threads/_list.blade.php
@@ -0,0 +1,23 @@
+@foreach ($threads as $thread)
+<article class="border p-4 mb-2 rounded">
+    <div class="flex justify-between">
+        <div class="flex flex-col">
+            <a href="{{$thread->path()}}" class="flex-1 text-2xl text-blue-500">
+                @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
+                <span class="font-semibold">{{$thread->title}}</span>
+                @else
+                {{$thread->title}}
+                @endif
+            </a>
+            <h4 class="text-sm">Posted By:
+                <a href="{{route('profile.show',$thread->creator->name)}}"
+                    class="text-blue-500">{{$thread->creator->name}}
+                </a>
+            </h4>
+        </div>
+        <p class="font-semibold">{{$thread->replies_count}} {{Str::plural('reply',$thread->replies_count)}}
+        </p>
+    </div>
+    <p class="p-2">{{$thread->body}}</p>
+</article>
+@endforeach
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,24 +5,9 @@
     <div class="flex justify-center px-6 my-12">
         <div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
             <h3 class="p-4 text-2xl text-center">Threads!</h3>
+            @include('threads._list')
 
-            @foreach ($threads as $thread)
-            <article class="border p-4 mb-2 rounded">
-                <div class="flex">
-                    <a href="{{$thread->path()}}" class="flex-1 text-2xl text-blue-500">
-                        @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                        <span class="font-semibold">{{$thread->title}}</span>
-                        @else
-                        {{$thread->title}}
-                        @endif
-                    </a>
-                    <p class="font-semibold">{{$thread->replies_count}} {{Str::plural('reply',$thread->replies_count)}}
-                    </p>
-                </div>
-                <p class="p-2">{{$thread->body}}</p>
-            </article>
-            @endforeach
-
+            {{$threads->links('pagination.default')}}
         </div>
     </div>
 </div>
